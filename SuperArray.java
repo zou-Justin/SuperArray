815f0c7221504ae69de825216d33322fcc9c6db0
Justin Dylan <justin82049@gmail.com>
2020-10-31 23:04:27 -0400

    added size and fixed constructor

--- SuperArray.java
+++ SuperArray.java
@@ -2,6 +2,10 @@ public class SuperArray{
   private String [] data;
   private int size;
   public SuperArray() {
-    
+    String Arg[] = new String[size];
+    size = 0;
+  }
+  public int size(){
+    return size;
   }
 }
