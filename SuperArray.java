44d5189d281f4fd3391d65d8eaafa2f7640d7e8b
Justin Dylan <justin82049@gmail.com>
2020-10-31 23:41:06 -0400

    added add

--- SuperArray.java
+++ SuperArray.java
@@ -2,11 +2,14 @@ public class SuperArray{
   private String [] data;
   private int size;
   public SuperArray() {
-    data = new String[size];
+    data = new String[10];
     size = 0;
   }
   public int size(){
     return size;
   }
-  
+  public boolean add(String element){
+    data[size+1] = element;
+  }
+
 }
