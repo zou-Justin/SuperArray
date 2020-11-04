bda23ea32cda7427f9bfc0837548e8d6b10bb2b1
Justin Dylan <justin82049@gmail.com>
2020-11-03 21:03:59 -0500

    added indexOf

--- SuperArray.java
+++ SuperArray.java
@@ -8,6 +8,7 @@ public class SuperArray{
   }
   public SuperArray(int InitialCapacity){
     data = new String[InitialCapacity];
+    size = 0;
   }
   public int size(){
     return size;
@@ -56,6 +57,17 @@ public boolean contains(String s){
   }
   return false;
 }
+public int indexOf(String s){
+  for (int i = 0;i < data.length;i++){
+    if (data[i].equals(s)){
+      return i;
+    }
+    else {
+      return -1;
+    }
+  }
+  return -1;
+}
   private void resize(){
     String[] Arg = new String[data.length * 2];
     for (int i = 0 ; i < data.length;i++){
@@ -65,8 +77,9 @@ public boolean contains(String s){
   }
   public void add(int index, String element){
     int newIndex = index;
-    for (int i = index; i < size - newIndex;i++){
-       data[newIndex - 1] = data[newIndex]  ;
+    size++;
+    for (int i = 0; i < size - newIndex + 1;i++){
+       data[index+1] = data[newIndex];
       newIndex++;
     }
     data[index] = element;
--- Tester.java
+++ Tester.java
@@ -1,32 +1,12 @@
 public class Tester{
   public static void main (String[]args){
+    SuperArray words = new SuperArray();
+words.add("kani");
+words.add("uni");
+words.add("ebi");
+words.add("kani");
+System.out.println(words.indexOf("a"));
+System.out.println(words);
 
-          int err = 0;
-          SuperArray a = new SuperArray();
-
-          err += check("a.size()", a.size(), 0);
-          err += check("a.add(\"test\")", a.add("test"), true);
-          err += check("a.get(0)", a.get(0), "test");
-          err += check("a.set(0, \"new\"", a.set(0, "new"), "test");
-          for (int i = 1; i < 15; i++) {
-              String name = "a.add(\"test" + i + "\")";
-              err += check(name, a.add("test" + i), true);
-          }
-          err += check("a.get(8)", a.get(8), "test8");
-          err += check("a.get(13)", a.get(13), "test13");
-
-          if (err == 0) System.out.println("All good!");
-          else if (err == 1) System.out.println("Uh oh... 1 error found.");
-          else System.out.println("Uh oh... " + err + " errors found.");
-      }
-
-      public static int check(String name, Object actual, Object expected) {
-          if (actual.equals(expected)) return 0;
-          else {
-              System.out.println("Failure on " + name + ": Expected \"" +
-                                  expected + "\", got \"" + actual + "\".");
-              return 1;
-          }
-      }
-
+}
 }
