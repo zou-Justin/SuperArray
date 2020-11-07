a29896c651988a57552d719a1131ce4b0c2d76c0
Justin Dylan <justin82049@gmail.com>
2020-11-06 20:27:17 -0500

    ClassNotes

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo{
+  public static void removeDuplicates(SuperArray s){  }
+  public static void main(String[]args){
+    SuperArray words = new SuperArray();
+    //grouped to save vertical space
+    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("una");    words.add("ebi");     words.add("toro");
+
+    System.out.println(words);
+    removeDuplicates(words);
+    System.out.println(words);  }
+}
