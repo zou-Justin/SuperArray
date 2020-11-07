d0544a00847f3b12b078326c5f7a8183fb7f3a95
Justin Dylan <justin82049@gmail.com>
2020-11-07 17:27:46 -0500

    first take

--- Demo.java
+++ Demo.java
@@ -1,12 +1,18 @@
 public class Demo{
-  public static void removeDuplicates(SuperArray s){  }
+  public static void removeDuplicates(SuperArray s){
+    s = new SuperArray();
+    for(int i = 0;i < s.size() -1 ;i++){
+      if (s.get(i).equals(s.get(i))) {
+        s.remove(i);
+      }
+    }
+   }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
-
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);  }
