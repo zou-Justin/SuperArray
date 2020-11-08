b0053de791ff97b75017c3c5a571fdbca57b1ce1
Justin Dylan <justin82049@gmail.com>
2020-11-07 20:35:07 -0500

    used indexOf for easier way

--- Demo.java
+++ Demo.java
@@ -1,20 +1,25 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
-    for(int i = 0;i < s.size();i++){
-      for (int j =0;j< s.size();j++){
-        if (s.get(i).equals(s.get(j))) {
+    for (int i = 0 ;i < s.size();){
+      if (s.indexOf(s.get(i)) != i){
         s.remove(i);
       }
+      else{
+        i++;
+      }
     }
-  }
 }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
-    //grouped to save vertical space
-    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("kani");
+    words.add("uni");
+     words.add("ebi");
+     words.add("una");
+    words.add("una");
+    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+  }
 }
